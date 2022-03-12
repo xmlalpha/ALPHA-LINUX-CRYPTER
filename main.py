ada5dab4c284c964be0921a3bd590db15b5f9048
alphaxml <101461024+xmlalpha@users.noreply.github.com>
2022-03-12 13:01:51 -1000

    Update main.py

--- main.py
+++ main.py
@@ -2,40 +2,53 @@
 
 import rsa
 import os
+from time import sleep
+k='#'
+j=0
+k='#'
+def fixed_space(i,array):
+    g=(' '*len(str(len(array))))
+    g=g.replace(' ','',len(str(int(i))))
+    return g
+def ani(i,array):
+    global k
+    #For accessing the global variables that are defined out of the function
+    global j
+    per=((i+1)*100)//len(array)
+    #To calculate percentage of completion of loop
+    c=per//5
+    #Integer division (the value 5 decides the length of the bar)
+    if c!=j:
+    #When ever the values of these 2 variables change add one # to the global variable k
+        k+='#'
+    y='['+k+'                     '+']'
+    #20 empty spaces (100/5) 
+    y=y.replace(' ','',len(k))
+    #To make the size of the bar fixed ever time the length of k increases one ' ' will be removed
+    g=fixed_space(per,array)
+    #To fix at the same position
+    f=fixed_space(i,array)
+    print('Loading : ',y,g+str(per)+'%',' ('+f+str(i+1)+' / '+str(len(array))+' ) ',end='\r')
+    #That same '\r' to clear previous text
+    j=c
 
-os.system('clear')
-print ("""
+array = range(100)
+for i in array:
+    ani(i,array)
+    sleep(0.1)
+    os.system("cls")
+
+
+def main():
+    print("""
  █████╗ ██╗     ██████╗ ██╗  ██╗ █████╗ 
 ██╔══██╗██║     ██╔══██╗██║  ██║██╔══██╗
 ███████║██║     ██████╔╝███████║███████║
 ██╔══██║██║     ██╔═══╝ ██╔══██║██╔══██║
 ██║  ██║███████╗██║     ██║  ██║██║  ██║
 ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝                     
-===========+coded by ALPHA+=============
-""")
+===========+coded by ALPHA+=============""")
 
-import time
-import sys
-
-done = 'false'
-#here is the animation
-def animate():
-    while done == 'false':
-        sys.stdout.write('\rloading |')
-        time.sleep(0.1)
-        sys.stdout.write('\rloading /')
-        time.sleep(0.1)
-        sys.stdout.write('\rloading -')
-        time.sleep(0.1)
-        sys.stdout.write('\rloading \\')
-        time.sleep(0.1)
-    sys.stdout.write('\rDone!     ')
-
-animate()
-#long process here
-done = 'false'      
-
-def main():
     choice = int(input("1. Encryption\n2. Decryption\n3. Creators\n4. GitHub\nChoose (1,2,3,4): "))
     if choice == 1:
         print("--+Encryption+--")
@@ -90,8 +103,7 @@ def cipher_decryption():
 
     print("Decrypted Text: {}".format(decryp_text))
     
-def credits():
-    os.system('clear') 
+def credits(): 
     message = print("OWNER: XML, CREDITS: BLACKHATPARROT")
 
 def github():
