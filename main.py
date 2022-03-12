45fb208042bb126de6983cc59358b328e9320a02
alphaxml <101461024+xmlalpha@users.noreply.github.com>
2022-03-12 09:13:15 -1000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import rsa
 import os
 
-os.system('clear')
+os.system('cls,clear')
 print ("""
    ▄████████  ▄█          ▄███████▄    ▄█    █▄       ▄████████ 
   ███    ███ ███         ███    ███   ███    ███     ███    ███ 
@@ -17,21 +17,57 @@ print ("""
 
 print()
 print("Loading...")
-publicKey, privateKey = rsa.newkeys(4096)
+publicKey, privateKey = rsa.newkeys(4096)        
 
-message = input("Enter your text>")
-print()
-print()
-encMessage = rsa.encrypt(message.encode(),
-						publicKey)
+def main():
+    choice = int(input("1. Encryption\n2. Decryption\nChoose(1,2): "))
+    if choice == 1:
+        print("---Encryption---")
+        cipher_encryption()
+    elif choice == 2:
+        print("---Decryption---")
+        cipher_decryption()
+    else:
+        print("Invalid Choice")
 
-print("original string>", message)
-print()
-print()
-print("encrypted string>", encMessage)
+def cipher_encryption():
+    message = input("Enter message: ")
+    key = 47
+    encryp_text = ""
 
-decMessage = rsa.decrypt(encMessage, privateKey).decode()
-print()
-print()
-print("decrypted string>", decMessage)
+    for i in range(len(message)):
+        temp = ord(message[i]) + key
+        if ord(message[i]) == 32:
+            encryp_text += " "
+        elif temp > 126:
+            temp -= 94
+            encryp_text += chr(temp)
+        else:
+            encryp_text += chr(temp)
+        # if-else
+    # for
+
+    print("Encrypted Text: {}".format(encryp_text))
+
+def cipher_decryption():
+    message = input("Enter message: ")
+    key = 47
+    decryp_text = ""
+
+    for i in range(len(message)):
+        temp = ord(message[i]) - key
+        if ord(message[i]) == 32:
+            decryp_text += " "
+        elif temp < 32:
+            temp += 94
+            decryp_text += chr(temp)
+        else:
+            decryp_text += chr(temp)
+            # if-else
+    # for
+
+    print("Decrypted Text: {}".format(decryp_text))
+    
+if __name__ == "__main__":
+    main()
  
