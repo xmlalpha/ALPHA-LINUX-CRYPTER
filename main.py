729a56272e1558c694db03c2e485724f43070bfc
xmlalpha <101461024+xmlalpha@users.noreply.github.com>
2022-03-12 03:21:09 -1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import rsa
+import os
+
+os.system('cls,clear')
+print ("""
+ █████╗ ██╗     ██████╗ ██╗  ██╗ █████╗ 
+██╔══██╗██║     ██╔══██╗██║  ██║██╔══██╗
+███████║██║     ██████╔╝███████║███████║
+██╔══██║██║     ██╔═══╝ ██╔══██║██╔══██║
+██║  ██║███████╗██║     ██║  ██║██║  ██║
+╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝
+                        coded by ALPHA
+""")
+
+print()
+print("Loading...")
+publicKey, privateKey = rsa.newkeys(4096)
+
+message = input("Enter your text>")
+print()
+print()
+encMessage = rsa.encrypt(message.encode(),
+						publicKey)
+
+print("original string>", message)
+print()
+print()
+print("encrypted string>", encMessage)
+
+decMessage = rsa.decrypt(encMessage, privateKey).decode()
+print()
+print()
+print("decrypted string>", decMessage)
+ 
