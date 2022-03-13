0bd1fa2734cb6e779feed95611b3eba9e0648ef7
alphaxml <101461024+xmlalpha@users.noreply.github.com>
2022-03-13 01:04:02 -1000

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,21 @@
-#!usr/bin/python3
-
-from secrets import choice
+import sys
 import rsa
 import os
 from time import sleep
-k='#'
-j=0
-k='#'
-def fixed_space(i,array):
-    g=(' '*len(str(len(array))))
-    g=g.replace(' ','',len(str(int(i))))
-    return g
-def ani(i,array):
-    global k
-    #For accessing the global variables that are defined out of the function
-    global j
-    per=((i+1)*100)//len(array)
-    #To calculate percentage of completion of loop
-    c=per//5
-    #Integer division (the value 5 decides the length of the bar)
-    if c!=j:
-    #When ever the values of these 2 variables change add one # to the global variable k
-        k+='#'
-    y='['+k+'                     '+']'
-    #20 empty spaces (100/5) 
-    y=y.replace(' ','',len(k))
-    #To make the size of the bar fixed ever time the length of k increases one ' ' will be removed
-    g=fixed_space(per,array)
-    #To fix at the same position
-    f=fixed_space(i,array)
-    os.system('clear')
-    print('Loading : ',y,g+str(per)+'%',' ('+f+str(i+1)+' / '+str(len(array))+' ) ',end='\r')
-    #That same '\r' to clear previous text
-    j=c
 
-array = range(100)
-for i in array:
-    ani(i,array)
-    sleep(0.1)
-    os.system("cls")
-
+def progress_bar(seconds):
+      for progress in range(0,seconds+1):
+        os.system('cls')  
+        percent = (progress * 100) // seconds
+        print("\n")
+        print("Loading...")
+        print("<" + ("=" * progress) + (" " * (seconds-progress)) + "> " + str(percent) + "%")
+        print("\n")
+        sleep(1)  
+   
+progress_bar(10)  
+os.system('cls')
+sleep(3)
 
 def main():
     print("""
@@ -144,11 +120,11 @@ def cipher_decryption():
 
     print("Decrypted Text: {}".format(decryp_text))
     
-def credits():
+def creators():
         print("OWNER: XML")
         print("CREDITS: BLACKHATPARROT")
 
-def creators():
+def github():
         print("https://github.com/xmlalpha")  
 
 if __name__ == "__main__":
